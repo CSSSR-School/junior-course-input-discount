21e42dac0647fc773b73022e49eeeca4ea687a39
Andrey Melnikov <andrey.melnikov@csssr.com>
2019-07-11 12:42:21 +0300

    feat(all): init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import s from "./index.module.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const Discount = () => {
+  return (
+    <>
+      <h3 className={s.title}>Скидка</h3>
+      <div className={s.wrapper}>
+        от <input className={s.discountValue} placeholder="30" />%
+      </div>
+    </>
+  );
+};
